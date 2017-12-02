16fa6b2813c7b3939441532c7888067af29ab530
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2017-12-02 06:28:55 -0200

    Set database envs and create user, difficulty and interest models

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
+require('dotenv').load();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('', '', '', {
-    host: '',
+const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
 
     pool: {
--- script/init.js
+++ script/init.js
@@ -2,17 +2,88 @@ const Sequelize = require('sequelize');
 var sequelize = require('../db');
 
 const User = sequelize.define('user', {
-    firstName: {
+    name: {
         type: Sequelize.STRING
     },
-    lastName: {
+    email: {
+        type: Sequelize.STRING,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING
+    },
+    picture: {
+        type: Sequelize.STRING
+    },
+    facebook_auth: {
+        type: Sequelize.STRING,
+        unique: true
+    },
+    google_auth: {
+        type: Sequelize.STRING,
+        unique: true
+    }
+});
+
+const Difficulty = sequelize.define('difficulty', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
         type: Sequelize.STRING
     }
 });
 
-User.sync({force: false}).then(() => {
-    return User.create({
-        firstName: 'Phelipe',
-        lastName: 'Rocha'
-    });
+const UserDifficulty = sequelize.define('userDifficulty', {
+    id_user: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'userInterestIndex'
+    },
+    id_difficulty: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'userInterestIndex'
+    }
 });
+
+User.belongsToMany(Difficulty, { through: 'userDifficulty', foreignKey: 'id_user' });
+Difficulty.belongsToMany(User, { through: 'userDifficulty', foreignKey: 'id_difficulty' });
+
+const Interest = sequelize.define('interest', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    image: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const UserInterest = sequelize.define('userInterest', {
+    id_user: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'userInterestIndex'
+    },
+    id_interest: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'userInterestIndex'
+    }
+});
+
+User.belongsToMany(Difficulty, { through: 'userInterest', foreignKey: 'id_user' });
+UserInterest.belongsToMany(User, { through: 'userInterest', foreignKey: 'id_interest' });
+
+Difficulty.sync({force: true});
+
+UserDifficulty.sync({force: true});
+
+Interest.sync({force: true});
+
+UserInterest.sync({force: true});
+
+User.sync({force: true});
